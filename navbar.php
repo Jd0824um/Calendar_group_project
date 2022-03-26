f43d1588e15fa51865dfdd7661fadc0ed7681d74
John Zika <JohnZikaJr@outlook.com>
2022-03-26 13:45:55 -0500

    bug
    bug

--- navbar.php
+++ navbar.php
@@ -4,16 +4,22 @@ session_start();
 <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
     <div class="container">
         <div class="navbar-header">
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html">Violin Lessons</a>
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.php">Violin Lessons</a>
         </div>
         <div class="collapse navbar-collapse" id="custom-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <?php
                 if (isset($_SESSION["authenticated"])) {
-                    echo ('<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
+                    echo ('
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Home</a>
                   <ul class="dropdown-menu">
                     <li><a href="index.php">Home</a></li>
                   </ul>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Profile</a>
+                    <ul class="dropdown-menu">
+                    <li><a href="profile.php">Profile</a></li>
+                    </ul>
                 </li>
                 <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Calendar</a>
                   <ul class="dropdown-menu">
@@ -21,10 +27,10 @@ session_start();
                   </ul>
                 </li>
                 <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Logout</a>
-                <ul class="dropdown-menu">
-                  <li><a href="logout.php">Logout</a></li>
-                </ul>
-              </li>');
+                    <ul class="dropdown-menu">
+                    <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                </li>');
                 } else {
                     echo ('<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Login</a>
                   <ul class="dropdown-menu">
