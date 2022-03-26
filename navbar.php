8e747550e584cdbef0d4a0bff2bb4c9b58882d77
John Zika <JohnZikaJr@outlook.com>
2022-03-26 13:50:27 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-    <div class="container">
+    <div>
         <div class="navbar-header">
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.php">Violin Lessons</a>
         </div>
