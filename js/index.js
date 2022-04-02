5d77a185aaba78d40318156675343547045d8453
Hayshell Rosario <hayshell.valdezrosario@my.metrostate.edu>
2022-04-02 16:28:57 -0500

    add Christian changes to index.js

--- js/index.js
+++ js/index.js
@@ -32,12 +32,12 @@ Cal.prototype.nextMonth = function()
 {
 	if (this.currMonth == 11)
 	{
-	this.currMonth = 0;
-	this.currYear = this.currYear + 1;
+		this.currMonth = 0;
+		this.currYear = this.currYear + 1;
 	}
 	else
 	{
-	this.currMonth = this.currMonth + 1;
+		this.currMonth = this.currMonth + 1;
 	}
 	this.showcurr();
 };
@@ -63,6 +63,7 @@ Cal.prototype.previousMonth = function()
 Cal.prototype.showcurr = function()
 {
 	this.showMonth(this.currYear, this.currMonth);
+	//alert("show curr");
 };
 
 
@@ -90,7 +91,7 @@ Cal.prototype.showMonth = function(y, m)
 	html += '<tr class="days">';
 	for(var i = 0; i < this.DaysOfWeek.length; i++)
 	{
-	html += '<td>' + this.DaysOfWeek[i] + '</td>';
+		html += '<td>' + this.DaysOfWeek[i] + '</td>';
 	}
 	html += '</tr>';
 
@@ -113,8 +114,8 @@ Cal.prototype.showMonth = function(y, m)
 		  var k = lastDayOfLastMonth - firstDayOfMonth+1;
 		  for (var j = 0; j < firstDayOfMonth; j++)
 		  {
-			html += '<td class="not-current">' + k + '</td>';
-			k++;
+			  html += '<td class="not-current">' + k + '</td>';
+			  k++;
 		  }
 	}
 
@@ -122,24 +123,28 @@ Cal.prototype.showMonth = function(y, m)
     var chk = new Date();
     var chkY = chk.getFullYear();
     var chkM = chk.getMonth();
-    if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay)
+	html += '<td class = "normal">' + '<b>' + i + '</b>';
+
+	for (var x = 0; x < num_students; x++)
 	{
-		html += '<td class="today">' + i; //+ '</td>';
-		for (var x = 0; x < 3; x++)
+		for (var b = 0; b < db_students[x].num_events; b++)
 		{
-			html += '<button type="button">Click Me!</button>'
+			if (db_students[x].db_events[b].date_year == y && db_students[x].db_events[b].date_month == m && db_students[x].db_events[b].date_day == i)
+			{
+				html += '<p></p>';
+				html += '<button type="button">';
+				var ttime_from = get_formatted_time(db_students[x].db_events[b].time_from);
+				var ttime_to = get_formatted_time(db_students[x].db_events[b].time_to);
+				html += ttime_from + '-' + ttime_to + ' ';
+				html += db_students[x].nam;
+				html += '</button>';
+			}
 		}
-		html += '</td>';
-    }
-	else
-	{
-		html += '<td class = "normal">' + '<b>' + i + '</b>';
-		for (var x = 0; x < 3; x++)
-		{
-			html += '<button type="button">Click Me!</button>'
-		}
-		html += '</td>';
-    }
+	}
+
+	html += '</td>';
+
+
     // If Saturday, closes the row
     if (dow == 6)
 	{
@@ -165,16 +170,40 @@ Cal.prototype.showMonth = function(y, m)
 
 	// Write HTML to the div
 	document.getElementById(this.divId).innerHTML = html;
+	//alert(num_students);
 };
+
+
+function get_formatted_time(ttime)
+{
+	var ampm = 'A';
+	var temp_hours = ttime.toString()[0] + ttime.toString()[1];
+	var temp_minutes = ttime.toString()[3] + ttime.toString()[4];
+	alert(ttime + ' ' + temp_hours);
+	if (temp_hours >= 12)
+	{
+		ampm = 'P';
+		if (temp_hours >= 13)
+			temp_hours -= 12;
+	}
+	else
+	if (temp_hours < 1)
+	{
+		temp_hours = 12;
+	}
+	
+	var ntime = temp_hours.toString() + ':' + temp_minutes.toString() + ampm;
+	//alert(ntime);
+	return ntime;
+}
 
 
 // On Load of the window
 window.onload = function()
 {
-	students();
+	display_students();
 	scheduler();
 	// Start calendar
-	var c = new Cal("divCal");			
 	c.showcurr();
 
 	// Bind next and previous button clicks
@@ -196,62 +225,320 @@ function getId(id)
 }
 
 
+var c = new Cal("divCal");			
 var db_students = [];
 var num_students = 0;
+var selected_student;
 
 
 class Student
 {
-  constructor(name)
-  {
-    this.nam = name;
-	this.gui_btn = null;
-	this.db_events = [];
-  }
+	constructor(tnam)
+	{
+		this.nam = tnam;
+		this.gui_btn = null;
+		this.num_events = 0;
+		this.db_events = [];
+	}
 }
 
 
 class Evnt
 {
-  constructor(tfrom, tto, date)
-  {
-    this.time_from = tfrom;
-	this.time_to = tto;
-	this.date = date;
-  }
+	//constructor(tnam, tfrom, tto, tyear, tmonth, tday)
+	constructor()
+	{
+		this.nam = "Event";
+		this.time_from = "";
+		this.time_to = "";
+		this.date_year = 2022;
+		this.date_month = 3;
+		this.date_day = 28;
+		this.notes = "";
+	}
 }
+
+
+function add_event()
+{
+	if (selected_student != null)
+	{
+		//**Get Form Values**//
+		var time_from = document.getElementById("time_from").value;
+		var time_to = document.getElementById("time_to").value;
+		var date_from = document.getElementById("date_from").value;
+		var date_to = document.getElementById("date_to").value;
+		var how_often = document.getElementById("how_often").value;
+		var day_of_week = document.getElementById("day_of_week").value;
+		var event_notes = document.getElementById("event_notes").value;
+
+		//Regex to replace dashes with fwd slashes, this is a weird confusing thing that otherwise will put the dates in the calendar 1 day early//
+		date_from = date_from.replace(/-/, '/')  // replace 1st "-" with "/"
+                             .replace(/-/, '/'); // replace 2nd "-" with "/"
+
+		if (how_often == "once")
+		{
+			var tevent = new Evnt();
+			tevent.nam = selected_student.nam;
+			tevent.date_year = new Date(date_from).getFullYear();
+			tevent.date_month = new Date(date_from).getMonth();
+			tevent.date_day = new Date(date_from).getDate();
+			tevent.time_from = time_from;
+			tevent.time_to = time_to;
+			tevent.notes = event_notes;
+			selected_student.db_events[selected_student.num_events] = tevent;
+			selected_student.num_events++;
+		}
+		else
+		if (how_often == "weekly")
+		{
+			//current_date.getDay() is 0-6 days of week
+			//getDate() is day of month (0-30 or whatever) //lala
+			var db_dates = get_dates(new Date(date_from), new Date(date_to));
+			for (var x = 0; x < db_dates.length; x++)
+			{
+				if (db_dates[x].getDay() == day_of_week)
+				{
+					var tevent = new Evnt();
+					tevent.nam = selected_student.nam;
+					tevent.date_year = db_dates[x].getFullYear();
+					tevent.date_month = db_dates[x].getMonth();
+					tevent.date_day = db_dates[x].getDate();
+					tevent.time_from = time_from;
+					tevent.time_to = time_to;
+					tevent.notes = event_notes;
+					selected_student.db_events[selected_student.num_events] = tevent;
+					selected_student.num_events++;
+				}
+			}
+		}
+		else
+		if (how_often == "biweekly")
+		{
+			var db_dates = get_dates(new Date(date_from), new Date(date_to));
+			var eo = false;
+			for (var x = 0; x < db_dates.length; x++)
+			{
+				if (db_dates[x].getDay() == day_of_week)
+				{
+					if (eo == false)
+					{
+						eo = true;
+						var tevent = new Evnt();
+						tevent.nam = selected_student.nam;
+						tevent.date_year = db_dates[x].getFullYear();
+						tevent.date_month = db_dates[x].getMonth();
+						tevent.date_day = db_dates[x].getDate();
+						tevent.time_from = time_from;
+						tevent.time_to = time_to;
+						tevent.notes = event_notes;
+						selected_student.db_events[selected_student.num_events] = tevent;
+						selected_student.num_events++;
+					}
+					else
+					//if (eo == true)
+					{
+						eo = false;
+					}
+				}
+			}
+		}
+		else
+		if (how_often == "monthly")
+		{
+			var db_dates = get_dates(new Date(date_from), new Date(date_to));
+			
+			var date_start = new Date(date_from);
+			var ndow = date_start.getDay();
+			var nweek = date_start.getDate();
+			//alert(date_start + " " + ndow);
+
+			nweek = nweek / 7;
+			if (nweek > 5)
+				nweek = 5;
+			else
+			if (nweek > 4)
+				nweek = 4;
+			else
+			if (nweek > 3)
+				nweek = 3;
+			else
+			if (nweek > 2)
+				nweek = 2;
+			else
+			if (nweek > 1)
+				nweek = 1;
+			else
+				nweek = 0;
+			
+			for (var x = 0; x < db_dates.length; x++)
+			{
+				if (db_dates[x].getDay() == ndow)
+				{
+					var tweek = db_dates[x].getDate();
+					tweek = tweek / 7;
+					if (tweek > 5)
+						tweek = 5;
+					else
+					if (tweek > 4)
+						tweek = 4;
+					else
+					if (tweek > 3)
+						tweek = 3;
+					else
+					if (tweek > 2)
+						tweek = 2;
+					else
+					if (tweek > 1)
+						tweek = 1;
+					else
+						tweek = 0;
+					
+					if (tweek == nweek)
+					{
+						var tevent = new Evnt();
+						tevent.nam = selected_student.nam;
+						tevent.date_year = db_dates[x].getFullYear();
+						tevent.date_month = db_dates[x].getMonth();
+						tevent.date_day = db_dates[x].getDate();
+						tevent.time_from = time_from;
+						tevent.time_to = time_to;
+						tevent.notes = event_notes;
+						selected_student.db_events[selected_student.num_events] = tevent;
+						selected_student.num_events++;
+					}
+				}
+			}
+		}
+		else
+		if (how_often == "daily")
+		{
+			//var db_dates = get_dates(new Date(), (new Date()).addDays(2));
+			var db_dates = get_dates(new Date(date_from), new Date(date_to));
+			for (var x = 0; x < db_dates.length; x++)
+			{
+				var tevent = new Evnt();
+				tevent.nam = selected_student.nam;
+				tevent.date_year = db_dates[x].getFullYear();
+				tevent.date_month = db_dates[x].getMonth();
+				tevent.date_day = db_dates[x].getDate();
+				tevent.time_from = time_from;
+				tevent.time_to = time_to;
+				tevent.notes = event_notes;
+				selected_student.db_events[selected_student.num_events] = tevent;
+				selected_student.num_events++;
+			}
+		}
+		selected_student.db_events.sort((a, b) => (a.time_from > b.time_from) ? 1 : -1)
+		c.showcurr();
+	}
+}
+
+
+Date.prototype.addDays = function(days)
+{
+	var dat = new Date(this.valueOf())
+	dat.setDate(dat.getDate() + days);
+	return dat;
+}
+
+
+function get_dates(date_from, date_to)
+{
+	var db_dates = new Array();
+	var current_date = date_from;
+	while (current_date <= date_to)
+	{
+		db_dates.push(current_date)
+		current_date = current_date.addDays(1);
+	}
+	return db_dates;
+}
+
+	/*
+	var dateArray = getDates(new Date(), (new Date()).addDays(2));
+	for (i = 0; i < dateArray.length; i ++ )
+	{
+		alert(dateArray[i]);
+	}
+	*/
 
 
 function add_student()
 {
-	//btn.style = 'width:100%;background-color:#40ff00;';
-	db_students[num_students] = new Student("John F");
-	num_students++;
-	students();
-	//alert(num_students);
+	var nam = document.getElementById("student_name").value;
+	if (nam.length > 0)
+	{
+		db_students[num_students] = new Student(nam);
+		selected_student = db_students[num_students];
+		num_students++;
+		display_students();	
+		c.showcurr();
+	}
 }
 
 
-function set_color(btn)
+function remove_student()
 {
-	btn.style = 'width:100%;background-color:#40ff00;';
+	if (selected_student != null)
+	{
+		//var tz = 0;
+		for (var z = 0; z < num_students; z++)
+		{
+			if (db_students[z].nam == selected_student.nam)
+			{
+				db_students[z] = db_students[num_students - 1];
+				db_students[num_students - 1] = null;
+				num_students--;
+				break;
+				//tz = z;
+				//alert("index: " + tz);
+			}
+		}
+	}
+	display_students();
 }
 
 
-function students()
+function select_student(btn)
+{
+	for (var z = 0; z < num_students; z++)
+	{
+		if (db_students[z].nam == btn.innerHTML)
+			selected_student = db_students[z];
+	}
+	display_students();
+	//btn.style = 'width:100%;background-color:#40ff00;';
+}
+
+
+function display_students()
 {
 	var html = "<table>";
+	//html += "<table>";
 	html += '<h1 style="font-size:1.5vw"><center>Students</center></h1>'
+	html += '<p></p>'
+	html += '<input type="text" id="student_name" name="student_name" placeholder="Student Name">'
+	html += '<p></p>'
 	html += '<button type="button" onclick="add_student()" style="background-color:black;color:green;width:3vw;height:3vw;font-size:2vw">+</button>';
-	html += '<button type="button" style="background-color:black;color:red;width:3vw;height:3vw;font-size:2vw">-</button><br>';
+	html += '<button type="button" onclick="remove_student()" style="background-color:black;color:red;width:3vw;height:3vw;font-size:2vw">-</button><br>';
 	
 	html += '<table style="background-color:#000000;"><tr></tr><tr><td>'
 
 	for (var x = 0; x < num_students; x++)
 	{
-		html += '<button onclick="set_color(this)" style="width:100%" type="button">'
-		html += db_students[x].nam;
-		html == '</button><br>';
+		if (db_students[x] == selected_student)
+		{
+			html += '<button onclick="select_student(this)" style="width:100%; background-color:#40ff00;" type="button">'
+			html += db_students[x].nam;
+			html == '</button><br>';
+		}
+		else
+		{
+			html += '<button onclick="select_student(this)" style="width:100%" type="button">'
+			html += db_students[x].nam;
+			html == '</button><br>';
+		}
 	}
 	
 	html += '</td></tr></table>'
@@ -264,14 +551,16 @@ function scheduler()
 {
 	var html = "<table>";
 	html += '<h1 style="font-size:1.5vw"><center>Schedule</center></h1>'
-	html += '<button type="button" style="background-color:black;color:green;width:3vw;height:3vw;font-size:2vw">+</button><br>';
-	html += '<label for="fname"></label><input type="time" value="13:00" id="fname" name="fname">'
-	html += '<label for="fname"></label><input type="time" value="13:30" id="fname" name="fname"><br>'
-	html += '<label for="often"></label><select name="often" id="often"><option value="every">Every</option><option value="every_other">Every Other</option><option value="once_per_month">Once Per Month</option><option value="once">Once</option></select><br>'
-	html += '<label for="days"></label><select name="day" id="days"><option value="monday">Monday</option><option value="tuesday">Tuesday</option><option value="wednesday">Wednesday</option><option value="thursday">Thursday</option><option value="friday">Friday</option><option value="saturday">Saturday</option><option value="sunday">Sunday</option></select><br>'
+	html += '<label for="time_from"></label><input type="time" value="13:00" id="time_from" name="time_from">'
+	html += '<label for="time_to"></label><input type="time" value="13:30" id="time_to" name="time_to"><br>'
+	html += '<label for="how_often"></label><select name="how_often" id="how_often"><option value="daily">Daily</option><option selected value="weekly">Weekly</option><option value="biweekly">Bi-Weekly</option><option value="monthly">Monthly</option><option value="once">One Time</option></select><br>'
+	html += '<label for="day_of_week"></label><select name="day_of_week" id="day_of_week"><option value="1">Monday</option><option value="2">Tuesday</option><option value="3">Wednesday</option><option value="4">Thursday</option><option value="5">Friday</option><option value="6">Saturday</option><option value="0">Sunday</option></select><br>'
 	html += '&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbspStart &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp End<br>';
-	html += '<label for="fname"></label><input type="date" id="fname" name="fname">'
-	html += '<label for="fname"></label><input type="date" id="fname" name="fname"><br>'
+	html += '<label for="date_from"></label><input type="date" value="2022-03-03" id="date_from" name="date_from">'
+	html += '<label for="date_to"></label><input type="date" value="2022-03-28" id="date_to" name="date_to"><br>'
+	html +=	'<textarea name="event_notes" id="event_notes" cols="40" placeholder="Additional Notes" rows="5"></textarea><br>'
+
+	html += '<button type="button" onclick="add_event()"  style="background-color:black;color:green;width:3vw;height:3vw;font-size:2vw">+</button><br>';
 
 	document.getElementById("divScheduler").innerHTML = html;
 }
